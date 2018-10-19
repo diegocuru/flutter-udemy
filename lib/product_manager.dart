b0b17bf51ab9f7f44ea8c4cf1b851063afc48fee
Diego Curumaco <dcurumaco@stsa.info>
2018-10-19 14:52:30 -0600

    First commit.

---
+++ lib/product_manager.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import './products.dart';
+
+class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager({this.startingProduct = 'Sweets Tester'}) {
+    print('[Products Manager] Constructor');
+  }
+ 
+  @override
+  State<StatefulWidget> createState() {
+    print('[Products Manager] createState()');
+    return _ProductManagerState();
+  }
+}
+
+class _ProductManagerState extends State<ProductManager> {
+  List<String> _products = [];
+
+  @override
+    void initState() {
+      super.initState();
+      print('[Products Manager] initState');
+      _products.add(widget.startingProduct);
+    }
+
+  @override
+    void didUpdateWidget(ProductManager oldWidget) {
+      print('[Products Manager] didUpdateWidget()');
+      super.didUpdateWidget(oldWidget);
+    }
+  @override
+  Widget build(BuildContext context) {
+    print('[Products Manager] build()');
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.all(10.0),
+          child: RaisedButton(
+            color: Theme.of(context).primaryColor,
+            onPressed: () {
+              setState(() {
+                _products.add('Ádvanced Food Tester');
+              });
+            },
+            child: Text('Add Product'),
+          ),
+        ),
+        Products(_products)
+      ],
+    );
+  }
+}
