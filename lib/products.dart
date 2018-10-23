79d6d957a345b51d0e86c68d10dc3a6dfb902f94
Diego Curumaco <dcurumaco@stsa.info>
2018-10-23 16:12:14 -0600

    Improvements conditions rendering

--- lib/products.dart
+++ lib/products.dart
@@ -9,20 +9,33 @@ class Products extends StatelessWidget {
 
   Widget _buildProductItem(BuildContext context, int index) {
     return Card(
-                  child: Column(
-                    children: <Widget>[
-                      Image.asset('assets/food.jpg'),
-                      Text(products[index])
-                    ],
-                  ),
-                );
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text(products[index])
+        ],
+      ),
+    );
   }
+
+  Widget _buildProductLists() {
+    Widget productCard;
+    if (products.length > 0) {
+      productCard = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    } else {
+      productCard = Center(
+        child: Text('No products found, please add some'),
+      );
+    }
+    return productCard;
+  }
+
   @override
   Widget build(BuildContext context) {
     print('[Products Widget] build()');
-    return products.length > 0 ? ListView.builder(
-      itemBuilder: _buildProductItem,
-      itemCount: products.length,
-    ) : Center(child: Text('No products found, please add some'));
+    return _buildProductLists();
   }
 }
