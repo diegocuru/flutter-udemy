19dd9b4a7a9d5173e1ee18d75250434782fad154
Diego Curumaco <dcurumaco@stsa.info>
2018-10-24 09:18:20 -0600

    Implementing basic navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './product_manager.dart';
+import './pages/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,13 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(brightness: Brightness.dark, primarySwatch: Colors.deepOrange, accentColor: Colors.deepPurple),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
-        ),
-        body: ProductManager(),
-      ),
+      theme: ThemeData(brightness: Brightness.light, primarySwatch: Colors.deepOrange, accentColor: Colors.deepPurple),
+      home: HomePage()
     );
   }
 }
