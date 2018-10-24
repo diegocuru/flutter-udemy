abdb890a533b0a13a46c7f94ebaa3e7dccfa757e
Diego Curumaco <dcurumaco@stsa.info>
2018-10-24 09:27:44 -0600

    Improvements the look for product page

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -8,12 +8,22 @@ class ProductPage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Product Detail'),
         ),
-        body: Column(children: <Widget>[
-          Text('Details!'),
-          RaisedButton(
-            child: Text('BACK'),
-            onPressed: () => Navigator.pop(context),
-          )
-        ]));
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('assets/food.jpg'),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text('Details!'),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text('BACK'),
+                  onPressed: () => Navigator.pop(context),
+                ),
+              )
+            ]));
   }
 }
