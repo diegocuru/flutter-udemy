09e71e9674a8c1c856cb9c036d7377ca222405ad
Diego Curumaco <dcurumaco@stsa.info>
2018-10-25 08:51:18 -0600

    Implements side drawer

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -5,12 +5,20 @@ import '../product_manager.dart';
 class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-      // TODO: implement build
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
-        ),
-        body: ProductManager(),
-      );
-    }
+    // TODO: implement build
+    return Scaffold(
+      drawer: Drawer(
+          child: Column(children: <Widget>[
+        AppBar(automaticallyImplyLeading: false, title: Text('Choose')),
+        ListTile(
+          title: Text('Manage Products'),
+          onTap: () {},
+        )
+      ])),
+      appBar: AppBar(
+        title: Text('EasyList'),
+      ),
+      body: ProductManager(),
+    );
+  }
 }
