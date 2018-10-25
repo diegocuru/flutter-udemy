9a1cf45ea67b10ff5458e9ff9a7fd25f82b9ba5d
Diego Curumaco <dcurumaco@stsa.info>
2018-10-25 09:39:26 -0600

    Assignment three

--- lib/pages/products_admin.dart
+++ lib/pages/products_admin.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 
+import './products.dart';
+
 class ProductsAdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+          child: Column(children: <Widget>[
+        AppBar(automaticallyImplyLeading: false, title: Text('Choose')),
+        ListTile(
+          title: Text('Products'),
+          onTap: () {
+            Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => ProductsPage()));
+          },
+        )
+      ])),
       appBar: AppBar(
         title: Text('Products Admin'),
       ),
       body: Center(
-        child: Text('Manage your products'),
+        child: Text('Hello'),
       ),
     );
   }
