8336e28407a133ed2146638ff800cb3c36ca05a7
Diego Curumaco <dcurumaco@stsa.info>
2018-10-25 14:36:11 -0600

    Saving user input

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -37,30 +37,33 @@ class ProductPage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return WillPopScope(
-        onWillPop: () {
-          Navigator.pop(context, false);
-          return Future.value(false);
-        },
-        child: Scaffold(
-            appBar: AppBar(
-              title: Text(title),
+      onWillPop: () {
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/food.jpg'),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text(title),
             ),
-            body: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Image.asset('assets/food.jpg'),
-                  Container(
-                    padding: EdgeInsets.all(10.0),
-                    child: Text(title),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(10.0),
-                    child: RaisedButton(
-                      color: Theme.of(context).accentColor,
-                      child: Text('DELETE'),
-                      onPressed: () => _showWarningDialog(context),
-                    ),
-                  )
-                ])));
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                color: Theme.of(context).accentColor,
+                child: Text('DELETE'),
+                onPressed: () => _showWarningDialog(context),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductCreatePageState();
+  }
+}
+
+class _ProductCreatePageState extends State<ProductCreatePage> {
+  String titleValue = '';
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Create Product'));
+    return Column(children: <Widget>[
+      TextField(
+        onChanged: (String value) {
+          setState(() {
+            titleValue = value;
+          });
+        },
+      ),
+      Text(titleValue),
+    ]);
   }
 }
