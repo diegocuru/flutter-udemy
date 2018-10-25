38566b82550d2859b8475d9f78d7f7e6a1b949ad
Diego Curumaco <dcurumaco@stsa.info>
2018-10-25 10:14:16 -0600

    Implements tabs

--- lib/pages/products_admin.dart
+++ lib/pages/products_admin.dart
@@ -1,26 +1,41 @@
 import 'package:flutter/material.dart';
 
 import './products.dart';
+import './product_create.dart';
+import './pdoruct_list.dart';
 
 class ProductsAdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-          child: Column(children: <Widget>[
-        AppBar(automaticallyImplyLeading: false, title: Text('Choose')),
-        ListTile(
-          title: Text('Products'),
-          onTap: () {
-            Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => ProductsPage()));
-          },
-        )
-      ])),
-      appBar: AppBar(
-        title: Text('Products Admin'),
-      ),
-      body: Center(
-        child: Text('Hello'),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        drawer: Drawer(
+            child: Column(children: <Widget>[
+          AppBar(automaticallyImplyLeading: false, title: Text('Choose')),
+          ListTile(
+            title: Text('Products'),
+            onTap: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) => ProductsPage()));
+            },
+          )
+        ])),
+        appBar: AppBar(
+          title: Text('Products Admin'),
+          bottom: TabBar(tabs: <Widget>[
+            Tab(icon: Icon(Icons.create), text: 'Create product'),
+            Tab(icon: Icon(Icons.create), text: 'Second tab'),
+          ]),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            ProductCreatePage(),
+            ProductListPage(),
+          ],
+        ),
       ),
     );
   }
