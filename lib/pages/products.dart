37e21a5b3a402b78c4a54d9b4c48301baca4aa48
Diego Curumaco <dcurumaco@stsa.info>
2018-10-26 14:25:00 -0600

    Adds icons for our sidemenu view

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -15,6 +15,7 @@ class ProductsPage extends StatelessWidget {
           child: Column(children: <Widget>[
         AppBar(automaticallyImplyLeading: false, title: Text('Choose')),
         ListTile(
+          leading: Icon(Icons.edit),
           title: Text('Manage Products'),
           onTap: () {
             Navigator.pushReplacementNamed(context, '/admin');
